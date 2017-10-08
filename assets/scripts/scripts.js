aa02cf8056a0ff04178854693b7a964a2e1c0c5b
Fernando Fuentes <fernando.fuentes1113@gmail.com>
2017-10-07 21:07:17 -0500

    updates

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,16 +1,22 @@
+// Global Variables
+// =====================================================
 // Create an array of Words
-var word = ["Jurassic", "Nintendo", "Guitars", "Javascript", "Houston", "StarWars", "Mexicans", "Tacos"];
-
-// Choose word randomly
-var random = Math.floor(Math.random() * word.length);
-var choosenWord = word[random];
+var word = ["guitars", "bear", "computers"];
+//Choose word randomly
+var randNum = Math.floor(Math.random() * word.length);
+var choosenWord = word[randNum];
+var rightWord = [];
+var wrongWord = [];
+var underScore = [];
 
 console.log(choosenWord);
 
+var docUnderScore = document.getElementsByClassName("underScores");
+var docRightGuess = document.getElementsByClassName("rightGuess");
 
-
+// Main
+// =====================================================
 // Create underScores based on length of word
-var underScore = [];
 var generateUnderscore = () => {
     for (var i = 0; i < choosenWord.length; i = i + 1) {
         underScore.push("_");
@@ -18,21 +24,26 @@ var generateUnderscore = () => {
     return underScore;
 };
 
-
-console.log(generateUnderscore())
+console.log(generateUnderscore());
 
 // Get users guess
 document.addEventListener("keypress", (event) => {
     var keyWord = String.fromCharCode(event.keyCode);
-
     // If users guess is right
     if (choosenWord.indexOf(keyWord) > -1) {
-        console.log(true);
-    }
+        // add to the rightWords array
+        rightWord.push(keyWord);
+        console.log(rightWord);
+
+        // Replace underScore to RightLetter
+        underScore[choosenWord.indexOf(keyWord)] = keyWord;
+        underScore[0].innerHTML = underScore.join(" ");
+
+        // If user guess is wrong
+
+        // Add to the wrongWords array
+    } else {
+        wrongWord.push(keyWord);
+        console.log(wrongWord);
+    };
 });
-
-
-// Check if guess is right
-
-// if right, push to right array
-// if wrong, push to wrong array
